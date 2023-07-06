c3cef8a96f2aa63cb407dbf37bbb2798e7c7eff7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-06 22:30:32 +0300

    Create Display_in_Tailwind.js

---
+++ Display_in_Tailwind.js
@@ -0,0 +1,87 @@
+In Tailwind CSS, the display utility classes are used to control the display behavior of elements. They allow you to specify how elements are rendered and how they interact with other elements on the page. Here are the display classes available in Tailwind CSS along with their corresponding CSS properties:
+
+block:
+
+display: block;
+Renders an element as a block-level element, taking up the full width of its parent container.
+inline-block:
+
+display: inline-block;
+Renders an element as an inline-level block element, allowing other elements to flow beside it while preserving block-level properties.
+inline:
+
+display: inline;
+Renders an element as an inline-level element, allowing other elements to flow beside it without causing line breaks.
+flex:
+
+display: flex;
+Turns an element into a flex container, allowing its children to be laid out in a flexible box model.
+inline-flex:
+
+display: inline-flex;
+Turns an element into an inline-level flex container, allowing its children to be laid out in a flexible box model.
+table:
+
+display: table;
+Renders an element as a table element.
+inline-table:
+
+display: inline-table;
+Renders an element as an inline-level table element.
+table-caption:
+
+display: table-caption;
+Renders an element as a table caption.
+table-cell:
+
+display: table-cell;
+Renders an element as a table cell.
+table-column:
+
+display: table-column;
+Renders an element as a table column.
+table-column-group:
+
+display: table-column-group;
+Renders an element as a table column group.
+table-footer-group:
+
+display: table-footer-group;
+Renders an element as a table footer group.
+table-header-group:
+
+display: table-header-group;
+Renders an element as a table header group.
+table-row-group:
+
+display: table-row-group;
+Renders an element as a table row group.
+table-row:
+
+display: table-row;
+Renders an element as a table row.
+flow-root:
+
+display: flow-root;
+Establishes a new block formatting context, containing floats and clearing any preceding floats.
+grid:
+
+display: grid;
+Turns an element into a grid container, allowing its children to be laid out in a grid format.
+inline-grid:
+
+display: inline-grid;
+Turns an element into an inline-level grid container.
+contents:
+
+display: contents;
+Makes an element's children appear as if they were direct children of the element's parent, effectively removing the element from the document structure.
+list-item:
+
+display: list-item;
+Renders an element as a list item.
+hidden:
+
+display: none;
+Hides an element from the layout, making it not appear on the page.
+These display classes provide flexibility in how elements are rendered and positioned within your layout. By applying the appropriate display utility class, you can achieve the desired visual and structural behavior for your elements in Tailwind CSS.
