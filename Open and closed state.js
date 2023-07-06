60d52ccd4356862e8a27537ac9cd2d33b3d71dcf
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-06 20:45:50 +0300

    Update Open and closed state.js

--- Open and closed state.js
+++ Open and closed state.js
@@ -1,4 +1,4 @@
-The example you've provided is using the <details> HTML element, which is used for creating a disclosure widget in which information is visible only when the widget is toggled into an "open" state. A <details> element typically comes with a <summary> element that represents the label for the disclosure widget.
+The example youve provided is using the <details> HTML element, which is used for creating a disclosure widget in which information is visible only when the widget is toggled into an "open" state. A <details> element typically comes with a <summary> element that represents the label for the disclosure widget.
 
 The HTML specification defines an open attribute for the <details> element, which indicates whether the details are being revealed or not. When the details are visible, the open attribute is present on the element, and when the details are hidden, the open attribute is absent.
 
@@ -6,7 +6,7 @@ In the context of Tailwind CSS, the open: modifier is used to conditionally appl
 
 In your example, the open:bg-white, dark:open:bg-slate-900, open:ring-1, open:ring-black/5, dark:open:ring-white/10, and open:shadow-lg classes are applied only when the <details> element is in an open state.
 
-Here's a breakdown of what each class does:
+Heres a breakdown of what each class does:
 
 open:bg-white and dark:open:bg-slate-900: These set the background color of the <details> element to white in light mode and slate-900 in dark mode when the element is open.
 open:ring-1, open:ring-black/5, and dark:open:ring-white/10: These add a ring (border) of width 1 around the <details> element with a color of black at 20% opacity in light mode and white at 10% opacity in dark mode when the element is open.
@@ -49,7 +49,7 @@ MORE EXPLANANTION
 
 When the <details> element is open, the open class is applied, and you can define specific styles for this state. In the example, different styles are applied to the <details> element when it is open using the open:bg-white, dark:open:bg-slate-900, open:ring-1, open:ring-black/5, dark:open:ring-white/10, open:shadow-lg, p-6, and rounded-lg classes.
 
-Here's a breakdown of the classes used in the example:
+Heres a breakdown of the classes used in the example:
 
 open:bg-white and dark:open:bg-slate-900: Sets the background color of the <details> element to white when it is open. If the dark mode is active, the background color is set to slate-900.
 
