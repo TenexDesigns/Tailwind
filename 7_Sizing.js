a01c36718d96d468b6b7fa883ebbc98adc82ba89
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-06 09:13:14 +0300

    Create 7_Sizing.js

---
+++ 7_Sizing.js
@@ -0,0 +1,61 @@
+Width
+
+The w-{size} utilities in Tailwind CSS control the width of an element. {size} can be a number from 0 to 96, 'auto', 'px', '1/2', '1/3', '2/3', '1/4', '2/4', '3/4', '1/5', '2/5', '3/5', '4/5', '1/6', '2/6', '3/6', '4/6', '5/6', 'full', or 'screen'.
+
+<div class="w-1/2">
+  <!-- half width -->
+</div>
+stackoverflow.com
+
+Min-Width
+
+The min-w-{size} utilities control the minimum width of an element. {size} can be '0', 'full', 'min', or 'max'.
+
+<div class="min-w-full">
+  <!-- minimum width of full -->
+</div>
+web.dev
+
+Max-Width
+
+The max-w-{size} utilities control the maximum width of an element. {size} can be 'none', '0', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', 'full', 'min', 'max', 'prose', or 'screen-sm/md/lg/xl/2xl'.
+
+<div class="max-w-md">
+  <!-- maximum width of md -->
+</div>
+ishadeed.com
+
+Height
+
+The h-{size} utilities control the height of an element. {size} can be a number from 0 to 96, 'auto', 'px', '1/2', '1/3', '2/3', '1/4', '2/4', '3/4', '1/5', '2/5', '3/5', '4/5', '1/6', '2/6', '3/6', '4/6', '5/6', 'full', or 'screen'.
+
+<div class="h-1/2">
+  <!-- half height -->
+</div>
+w3schools.com
+
+Min-Height
+
+The min-h-{size} utilities control the minimum height of an element. {size} can be '0', 'full', 'screen'.
+
+<div class="min-h-screen">
+  <!-- minimum height of screen -->
+</div>
+developer.mozilla.org
+
+Max-Height
+
+The max-h-{size} utilities control the maximum height of an element. {size} can be a number from 0 to 96, 'none', 'full', 'screen'.
+
+<div class="max-h-32">
+  <!-- maximum height of 32 -->
+</div>
+smashingmagazine.com
+
+
+
+
+
+
+
+----
