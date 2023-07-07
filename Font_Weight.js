b0a47216bb207fcede4f056cd8b99a0fe495055c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 14:04:49 +0300

    Create Font_Weight.js

---
+++ Font_Weight.js
@@ -0,0 +1,42 @@
+Font Weight
+Utilities for controlling the font weight of an element.
+
+​
+Quick reference
+Class
+Properties
+font-thin	font-weight: 100;
+font-extralight	font-weight: 200;
+font-light	font-weight: 300;
+font-normal	font-weight: 400;
+font-medium	font-weight: 500;
+font-semibold	font-weight: 600;
+font-bold	font-weight: 700;
+font-extrabold	font-weight: 800;
+font-black	font-weight: 900;
+​
+Basic usage
+​
+Setting the font weight
+Control the font weight of an element using the font-{weight} utilities.
+
+font-light
+The quick brown fox jumps over the lazy dog.
+
+font-normal
+The quick brown fox jumps over the lazy dog.
+
+font-medium
+The quick brown fox jumps over the lazy dog.
+
+font-semibold
+The quick brown fox jumps over the lazy dog.
+
+font-bold
+The quick brown fox jumps over the lazy dog.
+
+<p class="font-light ...">The quick brown fox ...</p>
+<p class="font-normal ...">The quick brown fox ...</p>
+<p class="font-medium ...">The quick brown fox ...</p>
+<p class="font-semibold ...">The quick brown fox ...</p>
+<p class="font-bold ...">The quick brown fox ...</p>
