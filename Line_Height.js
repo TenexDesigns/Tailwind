f0881e71119ac2d5aa16b27a59e1c9c2521fafb8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 14:18:02 +0300

    Create Line_Height.js

---
+++ Line_Height.js
@@ -0,0 +1,70 @@
+In Tailwind CSS, you can use utility classes to control the line height of text elements. Here are the utility classes for line height in Tailwind CSS along with their descriptions:
+
+leading-3:
+
+CSS Property: line-height: .75rem; /* 12px */
+Description: Sets the line height of the text to .75rem or 12 pixels.
+leading-4:
+
+CSS Property: line-height: 1rem; /* 16px */
+Description: Sets the line height of the text to 1rem or 16 pixels.
+leading-5:
+
+CSS Property: line-height: 1.25rem; /* 20px */
+Description: Sets the line height of the text to 1.25rem or 20 pixels.
+leading-6:
+
+CSS Property: line-height: 1.5rem; /* 24px */
+Description: Sets the line height of the text to 1.5rem or 24 pixels.
+leading-7:
+
+CSS Property: line-height: 1.75rem; /* 28px */
+Description: Sets the line height of the text to 1.75rem or 28 pixels.
+leading-8:
+
+CSS Property: line-height: 2rem; /* 32px */
+Description: Sets the line height of the text to 2rem or 32 pixels.
+leading-9:
+
+CSS Property: line-height: 2.25rem; /* 36px */
+Description: Sets the line height of the text to 2.25rem or 36 pixels.
+leading-10:
+
+CSS Property: line-height: 2.5rem; /* 40px */
+Description: Sets the line height of the text to 2.5rem or 40 pixels.
+leading-none:
+
+CSS Property: line-height: 1;
+Description: Sets the line height of the text to 1, which is the default line height.
+leading-tight:
+
+CSS Property: line-height: 1.25;
+Description: Sets a tight line height for the text, slightly closer than the default line height.
+leading-snug:
+
+CSS Property: line-height: 1.375;
+Description: Sets a snug line height for the text, slightly closer than leading-tight.
+leading-normal:
+
+CSS Property: line-height: 1.5;
+Description: Sets the normal line height for the text, which is the default line height.
+leading-relaxed:
+
+CSS Property: line-height: 1.625;
+Description: Sets a relaxed line height for the text, slightly looser than the default line height.
+leading-loose:
+
+CSS Property: line-height: 2;
+Description: Sets a loose line height for the text, providing extra spacing between lines.
+You can apply these utility classes directly to the HTML elements that contain text to control the line height. For example:
+
+html
+Copy code
+<p class="leading-6">This is a paragraph with a line height of 1.5rem.</p>
+In this example, the leading-6 class is applied to the paragraph element, setting the line height to 1.5rem or 24 pixels.
+
+These line height utility classes in Tailwind CSS allow you to easily adjust the spacing between lines of text and create visually appealing typography.
+
+
+
+  .....
