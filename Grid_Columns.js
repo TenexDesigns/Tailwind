e5a42e6a3088252b1d2e8366b05f48fffd3c9642
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 09:30:15 +0300

    Create Grid_Columns.js

---
+++ Grid_Columns.js
@@ -0,0 +1,11 @@
+Use the grid-cols-{n} utilities to create grids with n equally sized columns.
+
+
+
+e.g <div class="grid grid-cols-4 gap-4">
+  <div>01</div>
+  <!-- ... -->
+  <div>09</div>
+</div>
+
+
