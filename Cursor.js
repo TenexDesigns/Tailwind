a24b58bd39e1dc73eb1c011bdbdd40437b287587
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 21:32:16 +0300

    Create Cursor.js

---
+++ Cursor.js
@@ -0,0 +1,25 @@
+In Tailwind CSS, you can use the cursor utility classes to control the cursor appearance when hovering over an element. Here are some common cursor properties and their corresponding utility classes in Tailwind CSS:
+
+cursor-auto: Sets the cursor to the browser's default behavior.
+cursor-default: Sets the cursor to the default cursor.
+cursor-pointer: Sets the cursor to a pointing hand, indicating an interactive element.
+cursor-wait: Sets the cursor to an hourglass or spinner, indicating that the browser is busy.
+cursor-move: Sets the cursor to a move icon, indicating that the element can be dragged.
+cursor-not-allowed: Sets the cursor to a "not allowed" symbol, indicating that the element is disabled or cannot be interacted with.
+cursor-help: Sets the cursor to a question mark, indicating that help or more information is available.
+You can apply these cursor utility classes directly to your HTML elements. Here's an example of using the cursor utility classes in Tailwind CSS:
+
+html
+Copy code
+<button class="cursor-pointer">Click me</button>
+
+<a href="#" class="cursor-not-allowed">Disabled link</a>
+
+<div class="cursor-move">Drag me</div>
+In the example above, the first button will have a pointing hand cursor, indicating that it can be clicked. The second link will have a "not allowed" cursor, indicating that it is disabled. The third div will have a move cursor, indicating that it can be dragged.
+
+You can combine these cursor utility classes with other Tailwind CSS classes to customize the appearance and behavior of your elements.
+
+
+
+,,
