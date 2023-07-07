2ed2de1ea6488bd9e43b8d2de910b6b070e5c6f4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 09:23:04 +0300

    Create Flex_Shrink.js

---
+++ Flex_Shrink.js
@@ -0,0 +1,6 @@
+Class         Properties
+
+shrink	      flex-shrink: 1;      - Use shrink to allow a flex item to shrink if needed:
+shrink-0	    flex-shrink: 0;      - Use shrink-0 to prevent a flex item from shrinking:
+
+
