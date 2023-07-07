9a41ecfb61e9182b39765272fab44254d7b68269
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 09:14:46 +0300

    Create Flex_Grow.js

---
+++ Flex_Grow.js
@@ -0,0 +1,8 @@
+Class         Properties
+
+grow	        flex-grow: 1;         - Use grow to allow a flex item to grow to fill any available space:
+grow-0	      flex-grow: 0;         - Use grow-0 to prevent a flex item from growing:
+
+
+
+
