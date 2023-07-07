50c8290bd7e0bf127ea07eeb42de44de603cb6d7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 09:32:13 +0300

    Create Grid_Rows.js

---
+++ Grid_Rows.js
@@ -0,0 +1,11 @@
+Use the grid-rows-{n} utilities to create grids with n equally sized rows.
+
+
+<div class="grid grid-rows-4 grid-flow-col gap-4">
+  <div>01</div>
+  <!-- ... -->
+  <div>09</div>
+</div>
+
+
+  
