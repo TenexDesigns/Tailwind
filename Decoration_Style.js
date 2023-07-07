274f841ff3abdb9d36dd453de5c9b97a4e492105
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 14:35:43 +0300

    Create Decoration_Style.js

---
+++ Decoration_Style.js
@@ -0,0 +1,43 @@
+Text Decoration Style
+Utilities for controlling the style of text decorations.
+
+​
+Quick reference
+Class
+Properties
+decoration-solid	text-decoration-style: solid;
+decoration-double	text-decoration-style: double;
+decoration-dotted	text-decoration-style: dotted;
+decoration-dashed	text-decoration-style: dashed;
+decoration-wavy	text-decoration-style: wavy;
+​
+Basic usage
+​
+Setting the text decoration style
+Use the decoration-{style} utilities to change the style of an element’s text decoration.
+
+decoration-solid
+The quick brown fox jumps over the lazy dog.
+
+decoration-double
+The quick brown fox jumps over the lazy dog.
+
+decoration-dotted
+The quick brown fox jumps over the lazy dog.
+
+decoration-dashed
+The quick brown fox jumps over the lazy dog.
+
+decoration-wavy
+The quick brown fox jumps over the lazy dog.
+
+<p class="underline decoration-solid ...">The quick brown fox...</p>
+<p class="underline decoration-double ...">The quick brown fox...</p>
+<p class="underline decoration-dotted ...">The quick brown fox...</p>
+<p class="underline decoration-dashed ...">The quick brown fox...</p>
+<p class="underline decoration-wavy ...">The quick brown fox...</p>
+
+
+
+
+...
