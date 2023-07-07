6d8c5c3e921e9541ae33c05a8e9f73b2511ee790
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-07 14:03:48 +0300

    Create Italics.js

---
+++ Italics.js
@@ -0,0 +1,23 @@
+Font Style
+Utilities for controlling the style of text.
+
+​
+Quick reference
+Class
+Properties
+italic	font-style: italic;
+not-italic	font-style: normal;
+​
+Basic usage
+​
+Italicizing text
+The italic utility can be used to make text italic. Likewise, the not-italic utility can be used to display text normally — typically to reset italic text at different breakpoints.
+
+italic
+The quick brown fox jumps over the lazy dog.
+
+not-italic
+The quick brown fox jumps over the lazy dog.
+
+<p class="italic ...">The quick brown fox ...</p>
+<p class="not-italic ...">The quick brown fox ...</p>
