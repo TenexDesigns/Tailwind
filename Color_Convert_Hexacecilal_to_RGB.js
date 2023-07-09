7b7345c7d2b2a7157585bc311ad4508f113eca73
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-09 23:42:01 +0300

    Create Color_Convert_Hexacecilal_to_RGB.js

---
+++ Color_Convert_Hexacecilal_to_RGB.js
@@ -0,0 +1,32 @@
+To convert a hexadecimal color code to its RGB equivalent, you can follow these steps:
+
+Split the hexadecimal color code into three pairs of two characters each, representing the red, green, and blue components. For example, the color code #36B37E can be split into 36, B3, and 7E.
+
+Convert each pair from hexadecimal to decimal. In hexadecimal, each digit can range from 0 to F, which corresponds to decimal values from 0 to 15. You can use the following conversion table:
+
+Hexadecimal	Decimal
+0	0
+1	1
+2	2
+3	3
+4	4
+5	5
+6	6
+7	7
+8	8
+9	9
+A	10
+B	11
+C	12
+D	13
+E	14
+F	15
+Using this table, you can convert 36 to 54, B3 to 179, and 7E to 126.
+
+Combine the decimal values for the red, green, and blue components, respectively, to form the RGB notation. In this case, the RGB notation would be rgb(54, 179, 126).
+
+So, the conversion from #36B37E to rgb(54, 179, 126) involves splitting the hexadecimal code into three pairs, converting each pair to decimal, and combining the decimal values into the RGB notation.
+
+
+
+
