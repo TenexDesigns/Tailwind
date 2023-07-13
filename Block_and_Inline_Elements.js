b62674cc0cc220a08e0a3cd5f1dcf5523d996247
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-13 11:38:43 +0300

    Create Block_and_Inline_Elements.js

---
+++ Block_and_Inline_Elements.js
@@ -0,0 +1,52 @@
+
+     <! ---- **** HTML block level and inline elements **** ---- !>
+     HTML elements can be divided into two categories :
+       ---> block level 
+      ----> inline elements.
+     1.By default, block-level elements begin on new lines. e.g Three div elemnts
+     2. HTML block level elements can appear in the body of an HTML page.
+     3.It can contain another block level as well as inline elements.
+        
+        *******--- List of block level elements ----****
+              
+            p
+            h1, h2, h3, h4, h5, h6
+            ol, ul,li
+            pre
+            address
+            blockquote
+            dl
+            div
+            fieldset
+            form
+            hr
+            noscript
+            table
+      ******* ----- HTML inline elements -----*******
+      1. By default, inline elements do not begin on new lines.
+      They appear on the same line e.g Three span elemnts
+      ****** ----- List of inline elements ------ *****
+       span
+       button
+       label
+       a        --ancor tag
+       input
+       strong
+       textarea
+       select
+       img
+       script
+       map
+       object
+       acronym, abbr, cite, code, dfn, em, kbd, samp, var
+       b, big, i, small, tt, bdo, br, q, sub, sup 
+      How ever you can change the display of both inline and block elemnts by changing
+      The display in css.
+      e.g To change block elements to be displayed inline
+      blockelement{
+        display:inline-block;
+      }
+      To change inline elements to be displayed block
+       inlineelement{
+        display:block;
+       }
